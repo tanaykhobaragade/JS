66f2bb7ddca8f1c6da229cb4c974cc6afd93920b
Tanay Khobaragade <tanayk0892@gmail.com>
2021-06-15 16:51:51 +0530

    context in js

---
+++ 03Intermediate/03jsContext.js
@@ -0,0 +1,17 @@
+// Context in java Script
+
+// sayHello();
+// function sayHello(){
+//     console.log("Hello buddy!");
+// }
+// // sayHello();
+ 
+if (2 === "2"){
+    console.log("Hey is true")
+}
+
+// window. is global contecxt which is only run in browser not in node
+var myName = "Tanay";
+if(myName === window.myName){
+    console.log("A true Statement");
+}
