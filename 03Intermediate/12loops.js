47dd22a1457734e8edca70937f7c33d78a2fa673
Tanay Khobaragade <tanayk0892@gmail.com>
2021-06-15 17:16:30 +0530

    loops

---
+++ 03Intermediate/12loops.js
@@ -0,0 +1,47 @@
+
+// for(let i = 0; i < 10; i ++){
+//     console.log(i);
+// }
+
+const myState = ["Rajasthan", 
+"Maharashtra", 
+"Delhi", 
+"Assam", 
+1947,
+"Tamil nadu"
+];
+
+//------------------for loop-----------------------
+// for (let i = 0; i < myState.length; i++){
+//     // if (typeof myState[i] !== 'string') continue;
+//     // if (typeof myState[i] == 'string') continue;
+//     if (typeof myState[i] !== 'string') break;
+//     console.log(myState[i]);
+// }
+
+//-----------------while loop---------------
+
+// let i = 0;
+
+// while(i < myState.length){
+//     console.log(myState[i]);
+//     i++;
+// }
+
+//------------do-while loop------------
+
+// let i = 0;
+
+// do {
+//     console.log(myState[i]);
+//     i++;
+// }while(i < myState.length); 
+
+
+
+let i = 20;
+
+do {
+    console.log(i);
+    i++;
+}while(i < 10); 
