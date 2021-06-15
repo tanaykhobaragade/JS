fd0d47bc79350ffe7e6f41c9fe2d3c1b7c10b9f7
Tanay Khobaragade <tanayk0892@gmail.com>
2021-06-15 17:36:58 +0530

    login form

---
+++ Assignments/01assignment.js
@@ -0,0 +1,28 @@
+//01 ASSIGNMENT(YT Chapter 06):- First Name,Last name,Email,Password,Country,State,Course Count ,islogged in from google, is logged in from facebook  
+
+
+var userFirstName = "Tanay";
+var userLastName = "Khobaragade";
+var userEmail = "tanayk0892@gmail.com";
+var userPassword = "abc1212";
+var userConfirmPassword = "abc1212";
+var userCountry = "INDIA";
+var userState  = "Maharashtra";
+var userCourseCount = "0";
+var userIsLoggedInFromGoogle = true;
+var userIsLoggedInFromFacebook = false;
+
+
+console.log(`
+    User Details:
+    First Name = ${userFirstName}
+    Last Name = ${userLastName}
+    Email = ${userEmail}
+    Password = ${userPassword}
+    Country = ${userCountry}
+    State = ${userState}
+    Course Count = ${userCourseCount}
+    Google Login = ${userIsLoggedInFromGoogle}
+    Facebook Login = ${userIsLoggedInFromFacebook}
+
+`);
