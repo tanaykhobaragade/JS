e1a734314dd97ed624f73333a96daed31f9203bc
Tanay Khobaragade <tanayk0892@gmail.com>
2021-06-15 16:05:43 +0530

    building signup form

---
+++ 02 Basic/02Variables-two.js
@@ -0,0 +1,30 @@
+
+const uid = "abc123";
+// uid = "abcd1234";
+
+//building signup form
+var fullName = "Tanay Khobaragade"; //user name
+var email = "tanayk0892@gmail.com"; //user mail
+var password = "123456";            //user password
+var confirmPassword = "123456";
+var courseCount = 0;                //initially user would have 0 course enrolled
+var isLoggedInFromGoogle = false;   //user is logged in from google or mail.
+
+//to take input from user in pop up box
+//prompt("Enter your Name");
+
+//to display information use console.log();
+console.log("Full Name is:",fullName);
+console.log("your email is "+email);
+console.log(uid);
+console.log(`
+    with unique id : ${uid}
+    User is : ${fullName}
+    User's mail is : ${email}
+
+`)
+
+
+
+//ASSIGNMENT :- First Name, Last name, email,password, country,state,course count ,islogged in from google, is logged in from facebook  
+
