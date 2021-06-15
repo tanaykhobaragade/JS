a218581569965dffcefa7f9c8e7d90f6bf3bea27
Tanay Khobaragade <tanayk0892@gmail.com>
2021-06-15 16:01:59 +0530

    variables and it's output

---
+++ 02 Basic/01Variables.js
@@ -0,0 +1,19 @@
+var fullName = "Tanay Khobaragde";
+//how to name variables
+//fullName (camelCase)
+//full_name(snakeCase)
+
+//example of lco website
+var courseName = "MERN";
+var loggedCount = "25";
+var loggedIn = false;
+//Note:- if without double quotes-------console.log(loggedCount) it means it is referring to a varaible whose value should be displayed.
+//       if with double quotes----------console.log("loggedCount") it means it will just display what is in the double quotes.
+
+//undefined Datatype
+var paymentMode;
+console.log(paymentMode);
+ paymentMode = "DebitCard";
+ console.log(paymentMode);
+
+
