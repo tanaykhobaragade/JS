371f9a327c15ad21f8967ff6b3ff2c9e9b866b52
Tanay Khobaragade <tanayk0892@gmail.com>
2021-06-15 17:26:42 +0530

    this keyword advance

---
+++ 03Intermediate/15thisAgain.js
@@ -0,0 +1,22 @@
+//  for all regular function calls, this points to window object 
+console.log(this);
+
+var user = {
+    firstName: "Tanay",
+    courseCount: 4,
+    getCourseCount: function (){
+        console.log("Line 8", this);
+        function sayHello(){
+            console.log("Hello");
+            console.log("Line 11", this);
+        }
+        sayHello();
+    },
+};
+
+user.getCourseCount();
+
+function sayHello() {
+    console.log("HELLO");
+}
+sayHello();
