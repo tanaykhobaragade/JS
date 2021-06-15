3bbed754f65b8cabd77c2f9e6ba1e7aaa6c664f3
Tanay Khobaragade <tanayk0892@gmail.com>
2021-06-15 16:28:32 +0530

    temperature app using conditionals

---
+++ 02 Basic/04Conditionals.js
@@ -0,0 +1,20 @@
+//simple temperature app
+var temperature;
+
+//TODO: GO to Google and get Data
+
+temperature = 39;
+// var result = temperature < 20;
+// console.log (result);
+//conditional messages
+if (temperature <= 20) {
+console.log("it's very cold outside");
+}
+/*block of code is between curly braces {
+
+ }*/
+ if(temperature < 30){
+     console.log("it's moderate outside");
+ }else{
+     console.log("its really hot outside");
+ }
