4d477bbb72b3f56adfb8308eb096401d6d960daa
Tanay Khobaragade <tanayk0892@gmail.com>
2021-06-15 17:22:52 +0530

    forEach loop

---
+++ 03Intermediate/13loopsPartTwo.js
@@ -0,0 +1,10 @@
+// ----------------for each loop considered as method on the Array -------and technically not counted in loop---------
+const myState = ["Rajasthan", 
+"Maharashtra", 
+"Delhi", 
+"Assam", 
+1947,
+"Tamil nadu"
+];
+
+myState.forEach((e) => (console.log(e)));
