47258f0db80901452d3c86f7ffa319239a90eda2
Tanay Khobaragade <tanayk0892@gmail.com>
2021-06-15 17:38:46 +0530

    object basic

---
+++ Assignments/03assignment.js
@@ -0,0 +1,13 @@
+// object basic
+
+var device = {
+    deviceName : "Redmi Note 6 Pro",
+    deviceCode : "Tulip",
+    manufacture : "Xiaomi",
+    launchedYear : 2018,
+    version : "Android 11",
+    chipset : "SnapDragon 636",
+    price : 16000,
+};
+
+console.table(device);
