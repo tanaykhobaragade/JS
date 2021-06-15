474a946b5a7b7074a88c51b6ab8f3dac159e1163
Tanay Khobaragade <tanayk0892@gmail.com>
2021-06-15 16:42:46 +0530

    basic function

---
+++ 03Intermediate/01Functions.js
@@ -0,0 +1,27 @@
+// Function is a block of code which can be used it again and again
+
+function sayHello(name){
+    console.log("Hello there, Tanay");
+    console.log(`Hello there, ${name}. How are you`);
+}
+
+// when you want to call a function just put paranthesis ().
+// when you want to refere a function dont put paranthesis.
+
+
+// sayHello("Yash");
+// sayHello();
+
+function namestey(){
+    return "Hello in India";
+}
+
+namestey(); // here function is returning only value ......not displaying the value.
+
+var greetings = namestey();
+ console.log(greetings);
+ console.log(namestey());
+
+
+
+
