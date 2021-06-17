9aa9ce58130584f2015bdfba0f687288048d7ead
Tanay Khobaragade <tanayk0892@gmail.com>
2021-06-17 19:32:55 +0530

    upto 30 lecture concept practice

---
+++ Practice/01conceptsPractice.js
@@ -0,0 +1,22 @@
+/* what should be used in login form:-
+    console.log()----two methods
+    var,let,const,
+    operators (+ - * / %),
+    boolean values,
+    Math.round,
+    typeof,
+    conditionals---
+                    <, >, <=, >=, ==, !=, ===, !===, &&, || 
+                    if, if....else,switch
+                    ternary operator--- condition ? exprIfTrue : exprIfFalse
+    Functions,
+    Hoisting concept
+    ScopeChaining (Popsicle) 
+    array---pop, unshift, shift, Array.from, ___.indexOf
+    return keyword
+    filter, Slicing and Splicing,
+    Objects,
+    Loops:- for loop, while loop, do while loop, for of loop, for in loop
+    forEach
+    this keyword
+*/
