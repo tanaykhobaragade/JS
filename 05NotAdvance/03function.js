b015e22536645116f6fba2d15f6c26fa767409d4
Tanay Khobaragade <tanayk0892@gmail.com>
2021-06-27 19:45:33 +0530

    IIFE

---
+++ 05NotAdvance/03function.js
@@ -0,0 +1,6 @@
+// Self-Executing Anonymous Function OR IIFE
+// https://developer.mozilla.org/en-US/docs/Glossary/IIFE
+(function sayHello() {
+    console.log("Hello");
+    console.log("Hello there");
+})();
