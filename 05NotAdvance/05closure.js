8f3b023f8a172d21649036db41222d96480ba21e
Tanay Khobaragade <tanayk0892@gmail.com>
2021-06-28 14:34:07 +0530

    closure

---
+++ 05NotAdvance/05closure.js
@@ -0,0 +1,27 @@
+// closure---- 
+/* 
+function init(){
+    var firstName = "Tanay";
+    console.log("I am init");
+
+
+    function sayFirstName (){
+        console.log(firstName);
+    }
+    return sayFirstName;
+}
+
+var value = init();
+value();
+*/
+
+function doAddition(x){
+    return function (y){
+        return x+y;
+    };
+};
+
+var add5 = doAddition(4);
+console.log(add5(5));
+
+console.log(doAddition(5)(5));
