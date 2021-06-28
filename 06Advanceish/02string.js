a75aef0e8fc2628fdd13fe48d6b352f445d0bd43
Tanay Khobaragade <tanayk0892@gmail.com>
2021-06-28 22:43:27 +0530

    playing with strings

---
+++ 06Advanceish/02string.js
@@ -0,0 +1,7 @@
+var name = `Tanay`;
+var fullName = `${name} Khobragade`;
+console.log(fullName);
+console.log(fullName.charAt(0));
+console.log(fullName.endsWith("a"));
+console.log(fullName.includes("Tanay"));
+console.log(fullName.toUpperCase());
